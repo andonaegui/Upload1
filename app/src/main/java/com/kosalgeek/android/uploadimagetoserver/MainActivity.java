92ccbfef26d0b9102efc981a746de9b77e1b8072
Andonaegui <andonaegui@gmail.com>
2016-09-07 11:38:08 -0300

    Saca foto, la guarda con nombre unico, pero comenzo a fallar con entrar a galeria. Tiene comentarios importantes (mantenerlos)

--- app/src/main/java/com/kosalgeek/android/uploadimagetoserver/MainActivity.java
+++ app/src/main/java/com/kosalgeek/android/uploadimagetoserver/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,6 +19,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Iniciando la variable para obtener un Log.d() en la variable TAG en OnActivityResult
     private final String TAG = this.getClass().getName();
 
     //Declarando los Image View de los iconos
@@ -25,9 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     //declarando la libreria de photoutil del autor
     CameraPhoto cameraPhoto;
+    GalleryPhoto galleryPhoto;
 
     // este es un numero cualquiera para cumplir con los parametros pedidos en el camara request
     final int CAMERA_REQUEST = 13323;
+    final int GALLERY_REQUEST = 22131;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +52,13 @@ public class MainActivity extends AppCompatActivity {
         });
         //el contexto podria ser this, pero si estamos usando fragments es asi:
         cameraPhoto = new CameraPhoto(getApplicationContext());
+        galleryPhoto = new GalleryPhoto(getApplicationContext());
 
         //Con r.id.ivCamara buscamos por el id en el xml
+        ivImage = (ImageView) findViewById(R.id.ivImage);
         ivCamera = (ImageView) findViewById(R.id.ivCamera);
         ivGallery = (ImageView) findViewById(R.id.ivGallery);
         ivUpload = (ImageView) findViewById(R.id.ivUpload);
-        ivImage = (ImageView) findViewById(R.id.ivImage);
 
         // Ahora generamos un evento para la camara
         ivCamera.setOnClickListener(new View.OnClickListener() {
@@ -72,12 +78,44 @@ public class MainActivity extends AppCompatActivity {
                     }
             }
         });
+
+        ivGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Al clickear el gallery iniciamos la actividad y utilizamos el codigo de GRequest
+                startActivityForResult(galleryPhoto.openGalleryIntent(), GALLERY_REQUEST);
+            }
+        });
     }
     // Este es el result que obtenemos de la camara
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode == CAMERA_REQUEST){
-            cameraPhoto.getPhotoPath();
+        if(resultCode == RESULT_OK){
+            if(requestCode == CAMERA_REQUEST) {
+                //Al ejecutar obtenemos en consola el log (buscar por MainA...)
+                String photoPath = cameraPhoto.getPhotoPath();
+                //Obtenido de la libreria de OUM para poner la imagen como preview en la estrella
+                try {
+                    Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(512,512).getBitmap();
+                    ivImage.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "Algo salio mal mientras se cargaba la foto", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if (requestCode == GALLERY_REQUEST){
+                Uri uri = data.getData();
+                galleryPhoto.setPhotoUri(uri);
+                String photoPath = galleryPhoto.getPath();
+                try {
+                    Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(512,512).getBitmap();
+                    ivImage.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "Algo salio mal mientras se elegia la foto", Toast.LENGTH_SHORT).show();
+                }
+
+            }
         }
     }
 }
